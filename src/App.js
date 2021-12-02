229f4efba49d0d0c4f8adf650ec37b5855d63812
notGonim <mhmodgonim96@gmail.com>
2021-12-02 00:55:00 -1000

    done with the product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Button from "@mui/material/Button";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
+import ProductContainer from "./components/Product/ProductContainer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <ProductContainer/>
     </>
   );
 }
---
+++ src/components/Product/ProductContainer.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { Button, Grid, Typography } from "@mui/material";
+
+const ProductContainer = () => {
+  return (
+    <Box sx={{ minHeight: "60vh", mt: -23, mr: 2, ml: 2 }}>
+      <Grid container spacing={2} sx={{ mb: 4 }}>
+        <Grid
+          item
+          md={6}
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column",
+              lg: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+          }}
+        >
+          <Box
+            component="img"
+            src="images/products/f1.png"
+            sx={{ width: { xs: "100%", lg: "40%" } }}
+          />
+          <Box sx={{ pl: "10px", mt: { xs: 2, lg: 22 } }}>
+            <Typography sx={{ fontWeight: "bold" }} variant="h6">
+              Winter suits 2021
+            </Typography>
+            <Typography
+              sx={{ fontWeight: "bold", color: "#ff4081" }}
+              variant="h6"
+            >
+              $50.5
+            </Typography>
+            <Button variant="outlined" size="medium">
+              Add To Cart
+            </Button>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          md={6}
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column",
+              lg: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+          }}
+        >
+          <Box
+            component="img"
+            src="images/products/f2.png"
+            sx={{ width: { xs: "100%", lg: "40%" } }}
+          />
+          <Box sx={{ pl: "10px", mt: { xs: 2, lg: 22 } }}>
+            <Typography sx={{ fontWeight: "bold" }} variant="h6">
+              New Styles 2021
+            </Typography>
+            <Typography
+              sx={{ fontWeight: "bold", color: "#ff4081" }}
+              variant="h6"
+            >
+              $105.77
+            </Typography>
+            <Button variant="outlined" size="medium">
+              Add To Cart
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default ProductContainer;

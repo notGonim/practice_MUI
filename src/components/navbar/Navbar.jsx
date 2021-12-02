d259fbefd04b5ab095e10b722a2868010adddfc9
notGonim <mhmodgonim96@gmail.com>
2021-12-01 23:15:41 -1000

    Done with the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -6,8 +6,20 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const Navbar = () => {
+   const [anchorEl, setAnchorEl] = useState(null);
+   const open = Boolean(anchorEl);
+   const handleClick = (event) => {
+     setAnchorEl(event.currentTarget);
+   };
+   const handleClose = () => {
+     setAnchorEl(null);
+   };
+
+   
   return (
     <AppBar sx={{ backgroundColor: "white" }}>
       <Toolbar>
@@ -43,10 +55,28 @@ const Navbar = () => {
               Shop
             </Typography>
             <Typography
+              aria-controls="category-menu"
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
               sx={{ marginRight: "10px", cursor: "pointer", color: "black" }}
             >
               Categories
             </Typography>
+            {/* Category Items */}
+            <Menu
+              id="category-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>Men</MenuItem>
+              <MenuItem onClick={handleClose}>Woman</MenuItem>
+              <MenuItem onClick={handleClose}>Childrent</MenuItem>
+            </Menu>
             <Typography
               sx={{ marginRight: "10px", cursor: "pointer", color: "black" }}
             >
